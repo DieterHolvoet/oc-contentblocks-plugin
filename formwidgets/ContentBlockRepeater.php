3e636bd476a85eddc5c8050216bc40b9e3463fee
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-04-01 22:21:53 +0200

    Update custom repeater widget to work with latest version of OctoberCMS

--- formwidgets/ContentBlockRepeater.php
+++ formwidgets/ContentBlockRepeater.php
@@ -23,80 +23,18 @@ class ContentBlockRepeater extends Repeater
 
     public function guessViewPath($suffix = '', $isPublic = false)
     {
-        // Below line changed
         $class = parent::class;
         return $this->guessViewPathFrom($class, $suffix, $isPublic);
     }
 
     protected function makeItemFormWidget($index = 0, $groupCode = null)
     {
-        $className = $this->contentBlockDefinitions->getClassName($groupCode);
-        $configDefinition = $this->useGroups
-            ? $this->getGroupFormFieldConfig($groupCode)
-            : $this->form;
+        $data = $this->getValueFromIndex($index);
 
-        $config = $this->makeConfig($configDefinition);
-        $data = $this->getLoadValueFromIndex($index);
-
-        // Below line changed
-        $config->model = $config->data = empty($data) ? new $className : $data;
-        $config->alias = $this->alias . 'Form'.$index;
-        $config->arrayName = $this->getFieldName().'['.$index.']';
-        $config->isNested = true;
-
-        $widget = $this->makeWidget('Backend\Widgets\Form', $config);
-        $widget->bindToController();
-
-        $this->indexMeta[$index] = [
-            'groupCode' => $groupCode
-        ];
-
-        return $this->formWidgets[$index] = $widget;
-    }
-
-    protected function processExistingItems()
-    {
-        $loadedIndexes = $loadedGroups = [];
-        $loadValue = $this->getLoadValue();
-
-        // Ensure that the minimum number of items are preinitialized
-        // ONLY DONE WHEN NOT IN GROUP MODE
-        if (!$this->useGroups && $this->minItems > 0) {
-            if (!is_array($loadValue)) {
-                $loadValue = [];
-                for ($i = 0; $i < $this->minItems; $i++) {
-                    $loadValue[$i] = [];
-                }
-            } elseif (count($loadValue) < $this->minItems) {
-                for ($i = 0; $i < ($this->minItems - count($loadValue)); $i++) {
-                    $loadValue[] = [];
-                }
-            }
+        if (empty($groupCode) && is_object($data)) {
+            $groupCode = $this->contentBlockDefinitions->getShortName(get_class($data));
         }
 
-        if (is_array($loadValue)) {
-            foreach ($loadValue as $index => $loadedValue) {
-                $loadedIndexes[] = $index;
-                // Below line changed
-                $loadedGroups[] = $this->contentBlockDefinitions->getShortName($loadedValue);
-            }
-        }
-
-        $itemIndexes = post($this->indexInputName, $loadedIndexes);
-        $itemGroups = post($this->groupInputName, $loadedGroups);
-
-        if (!count($itemIndexes)) {
-            return;
-        }
-
-        $items = array_combine(
-            (array) $itemIndexes,
-            (array) ($this->useGroups ? $itemGroups : $itemIndexes)
-        );
-
-        foreach ($items as $itemIndex => $groupCode) {
-            $this->makeItemFormWidget($itemIndex, $groupCode);
-            $this->indexCount = max((int) $itemIndex, $this->indexCount);
-        }
+        return parent::makeItemFormWidget($index, $groupCode);
     }
 }
