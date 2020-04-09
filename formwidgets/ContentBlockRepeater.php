1d904c3a75117c5c70f031518edc6af0fe3b7155
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-04-09 17:50:09 +0200

    Update custom repeater widget to work with filterFields

--- formwidgets/ContentBlockRepeater.php
+++ formwidgets/ContentBlockRepeater.php
@@ -27,14 +27,55 @@ class ContentBlockRepeater extends Repeater
         return $this->guessViewPathFrom($class, $suffix, $isPublic);
     }
 
+    /**
+     * The applied modifications will:
+     * - resolve the group code from the model name if missing
+     * - replace a new array value with a content block model
+     * - replace the widget data with the content block model, this is allowed
+     *  because array access on models is possible
+     * - replace the widget model with the content block model, so methods like
+     * filterFields are triggered on the content block model instead of the page model
+     */
     protected function makeItemFormWidget($index = 0, $groupCode = null)
     {
-        $data = $this->getValueFromIndex($index);
+        $value = $this->getValueFromIndex($index);
 
-        if (empty($groupCode) && is_object($data)) {
-            $groupCode = $this->contentBlockDefinitions->getShortName(get_class($data));
+        if (is_object($value)) {
+            $block = $value;
+
+            if (empty($groupCode)) {
+                $groupCode = $this->contentBlockDefinitions->getShortName(get_class($value));
+            }
         }
 
-        return parent::makeItemFormWidget($index, $groupCode);
+        if (is_array($value)) {
+            $className = $this->contentBlockDefinitions->getClassName($groupCode);
+            $block = new $className;
+            $block->unguard();
+            $block->fill($value);
+        }
+
+        $configDefinition = $this->useGroups
+            ? $this->getGroupFormFieldConfig($groupCode)
+            : $this->form;
+
+        $config = $this->makeConfig($configDefinition);
+        $config->model = $config->data = $block;
+        $config->alias = $this->alias . 'Form' . $index;
+        $config->arrayName = $this->getFieldName().'['.$index.']';
+        $config->isNested = true;
+        if (self::$onAddItemCalled || $this->minItems > 0) {
+            $config->enableDefaults = true;
+        }
+
+        $widget = $this->makeWidget('Backend\Widgets\Form', $config);
+        $widget->previewMode = $this->previewMode;
+        $widget->bindToController();
+
+        $this->indexMeta[$index] = [
+            'groupCode' => $groupCode
+        ];
+
+        return $this->formWidgets[$index] = $widget;
     }
 }
